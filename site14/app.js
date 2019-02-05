77f18a4cb52b1beb3cf3d02cc826672ab950864a
mateoblandon <matius-b@hotmail.com>
2019-02-04 20:39:34 -0500

    Entrada a Javascript

---
+++ site14/app.js
@@ -0,0 +1,8 @@
+var main = function () {
+ "use strict";
+ $(".comment-input button").on("click", function (event) {
+ $(".comments").append("<p>this is a new comment</p>");
+
+ });
+};
+$(document).ready(main);
